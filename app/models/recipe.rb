c2c5505d0de0104b31ac7f43c7b3848865aae8f4
Yan Wang <Yan@Flatirons-MacBook-Air-9.local>
2018-09-05 11:25:17 -0400

    Done.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,11 @@
 class Recipe < ActiveRecord::Base
-  has_many :ingredients 
+  has_many :ingredients
+  accepts_nested_attributes_for :ingredients
+
+  def ingredients_attributes=(ingredients_attributes)
+    # byebug
+    ingredients_attributes.each do |i, attribute|
+       self.ingredients << Ingredient.find_or_create_by(attribute)
+    end
+  end
 end
